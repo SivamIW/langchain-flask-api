ef0d4d175ab47e683844de3b3301acdcefaef5a4
SivamIW <sivam@impactweaver.com>
2024-07-01 23:37:17 +0530

    chore: Improve installation process and error handling in run_server.bat

--- run_server.bat
+++ run_server.bat
@@ -1,3 +1,12 @@
 @echo off
-pip install -r requirements.txt
+echo Installing requirements...
+pip install -r requirements.txt -q
+if %errorlevel% neq 0 (
+    echo Error occurred during installation:
+    pip install -r requirements.txt
+    pause
+    exit /b %errorlevel%
+)
+echo Requirements installed successfully.
+echo Starting server...
 python server.py
